80d33a7fd6f2fceb7da5ff126a77a843fe38e761
ikmsuarez23 <ikmsuarez23@lhusurbil.eus>
2026-01-12 13:07:16 +0100

    first commit

---
+++ demo/src/main/java/com/model/JugadorPartida.java
@@ -0,0 +1,37 @@
+package com.model;
+
+import jakarta.persistence.*;
+
+
+    @Entity
+@Table(name = "jugador_partida")
+public class JugadorPartida {
+
+    @EmbeddedId
+    private JugadorPartidaID id = new JugadorPartidaID();
+
+    @ManyToOne
+    @MapsId("jugadorId")
+    private Jugador jugador;
+
+    @ManyToOne
+    @MapsId("partidaId")
+    private Partida partida;
+
+    private int score;
+
+    // Getters y setters
+    public JugadorPartidaID getId() { return id; }
+    public void setId(JugadorPartidaID id) { this.id = id; }
+
+    public Jugador getJugador() { return jugador; }
+    public void setJugador(Jugador jugador) { this.jugador = jugador; }
+
+    public Partida getPartida() { return partida; }
+    public void setPartida(Partida partida) { this.partida = partida; }
+
+    public int getScore() { return score; }
+    public void setScore(int score) { this.score = score; }
+
+    
+}
